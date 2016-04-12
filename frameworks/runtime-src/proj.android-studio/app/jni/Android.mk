87e826c810f2eb022223a197fbc242f36dd8d67e
Nick <thuydx55vnu@gmail.com>
2016-04-12 15:25:29 +0700

    init project android and update submodules

--- frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
+++ frameworks/runtime-src/proj.android-studio/app/jni/Android.mk
@@ -6,12 +6,17 @@ LOCAL_MODULE := cocos2djs_shared
 
 LOCAL_MODULE_FILENAME := libcocos2djs
 
-LOCAL_SRC_FILES := hellojavascript/main.cpp \
-                   ../../../Classes/AppDelegate.cpp 
+FILE_LIST := $(wildcard $(LOCAL_PATH)/../../../Classes/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/../../../Classes/js-bindings/*.cpp)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+LOCAL_SRC_FILES := hellojavascript/main.cpp
+LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
+					$(LOCAL_PATH)/../../../Classes/js-bindings
 
 LOCAL_STATIC_LIBRARIES := cocos2d_js_static
+LOCAL_STATIC_LIBRARIES += cocos2d_facebookx_static
 
 LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=2 -DCOCOS2D_JAVASCRIPT
 
@@ -19,3 +24,4 @@ include $(BUILD_SHARED_LIBRARY)
 
 
 $(call import-module, scripting/js-bindings/proj.android)
+$(call import-module, ../../FacebookX)
